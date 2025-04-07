31145ef21cdb094d2c017bc4786b5990950006d5
kuzmingeorgii <kuzmin098765@gmail.com>
2025-04-08 01:17:57 +0300

    список профессий

--- server.py
+++ server.py
@@ -19,5 +19,21 @@ def training(prof):
     return render_template('training.html', title=title, image=image)
 
 
+@app.route('/ol/<list_type>')
+def list_prof(list_type):
+    professions = [
+        "инженер-исследователь", "пилот", "строитель", "экзобиолог",
+        "врач", "инженер по терраформированию", "климатолог",
+        "специалист по радиационной защите", "астрогеолог", "гляциолог",
+        "инженер жизнеобеспечения", "метеоролог", "оператор марсохода",
+        "киберинженер", "штурман", "пилот дронов"
+    ]
+
+    if list_type not in ['ol', 'ul']:
+        list_type = None
+
+    return render_template('list_prof.html', list_type=list_type, professions=professions)
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8008)

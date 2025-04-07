3e214da65e92e581790dd0cc4aa00ff14e6de299
kuzmingeorgii <kuzmin098765@gmail.com>
2025-04-08 01:04:02 +0300

    трейининг шаблон

--- server.py
+++ server.py
@@ -9,5 +9,15 @@ def index(title=""):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    is_engineer = 'инженер' in prof.lower() or 'строитель' in prof.lower()
+
+    title = "Инженерные тренажеры" if is_engineer else "Научные симуляторы"
+    image = "engineer.jpg" if is_engineer else "scientist.jpg"
+
+    return render_template('training.html', title=title, image=image)
+
+
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8008)
